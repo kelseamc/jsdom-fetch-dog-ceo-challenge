8ab17f797b93fea62e3b788174716fc9092eb858
kelseamc <kelsea.mcall@gmail.com>
2020-12-16 20:26:22 -0500

    commit tree 97ec174c36cb3ac0ba96f84fb5506d210691e6ca

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+function renderAllImages(url) {
+    const img = document.createElement("img")
+    const div = document.querySelector("div#dog-image-container")
+    img.src = url
+    div.append(img)
+} 
+
+
+// ********** On Page Load ***********
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then((imageArray) => {
+        console.log(imageArray);
+        imageArray.message.forEach ((imageUrl) => {
+            renderAllImages(imageUrl);
+        });
+    
+    })
+
+    
